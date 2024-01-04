4c868c675b1d7941ac41bcaeaf2b8b2d63a861f4
Jatin Jayadev <jatin.batchu@kalvium.community>
2024-01-05 01:19:44 +0530

    Pushing

--- app.js
+++ app.js
@@ -2,35 +2,48 @@
 
 function fetchRandomMeal() {
     fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    
     .then(result => {
       return result.json();
     })
+    
     .then(finalresponse => {
       let randomMeal=finalresponse.meals[0]
       displayRandomMeal(randomMeal)
       console.log(finalresponse.meals[0])
-
     })
+    
     .catch(error => {
       console.log('Error:', error);
     });
+
 }
 fetchRandomMeal();
 
 function displayRandomMeal(random){
     let randomMealDiv=document.getElementById("randomMealDiv")
+    
     randomMealDiv.innerHTML+=`
     <div class="result-div" id="Modal-open">
     <img class="dish-image" src="${random.strMealThumb}" alt="">
     <h3 class="orange" id="randomDishName">${random.strMeal}</h3>
     </div>`
+    
+    let mealId=random.idMeal
+    randomMealDiv.addEventListener('click',()=>{
+        openpopup(mealId)
+        console.log(mealId)
+    })
 }
 
 
 let submitButton=document.getElementById("submit")
+
 submitButton.addEventListener('click',()=>{
+    
     let inputCategory=document.getElementById("input").value
     console.log(inputCategory)
+    
     fetchSearchedMeal(inputCategory)
 })
 
@@ -53,13 +66,27 @@ function fetchSearchedMeal(category){
         
         displayDish.innerHTML=""
         searchedCategory.forEach(dishes=>{
-            displayDish.innerHTML+=`
-            <div class="result-div">
+            const div=document.createElement("div")
+            
+            div.innerHTML=`
+            <div class="result-div" id="${dishes.idMeal}" >
                 <img class="dish-image" id="Modal-open" src="${dishes.strMealThumb}" alt="">
                 <h3 class="orange">${dishes.strMeal}</h3>
             </div>
             `
-            console.log(dishes.strMealThumb)
+            div.onclick=()=>{
+                console.log("no")
+                openpopup(dishes.idMeal)
+            }
+            
+            displayDish.append(div)
+            
+            // console.log(dishes.idMeal)
+            // let mealId=document.getElementById(dishes.idMeal)
+            // document.getElementById(dishes.idMeal).addEventListener('click',()=>{
+            //     console.log(mealId)
+            //     openpopup(dishes.idMeal)
+            // })
         })
     })
     .catch(error=>{
@@ -68,11 +95,49 @@ function fetchSearchedMeal(category){
 }
 
 let modalOpen=document.getElementById("Modal-open")
-modalOpen.addEventListener('click',()=>{
-    document.getElementById("modal").style.display="block"
-})
+// modalOpen.addEventListener('click',()=>{
+//     document.getElementById("modal").style.display="block"
+// })
+let modalIngredients=document.getElementById("modalIngredients")
 
 let modalClose=document.getElementById("closeBtn")
 modalClose.addEventListener('click',()=>{
     document.getElementById("modal").style.display="none"
 })
+
+function openpopup(mealId){
+    
+    console.log(mealId)
+    
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    
+    .then(result=>{
+        return result.json()
+    })
+    
+    .then(response=>{
+        // console.log(response.meals[0])
+        let dishes=response.meals[0]
+
+        modalIngredients.innerHTML=""
+        
+        for(let i=1;i<21;i++){
+            let ingredient = dishes[`strIngredient${i}`];
+            
+            if (ingredient !== "" ) {
+                console.log(`${ingredient}`);
+                modalIngredients.innerHTML+=`
+                <li>${ingredient}</li>`
+            }
+        }
+        
+        document.getElementById("modal").style.display="block"
+    })
+}
+// modalOpen.addEventListener('click',()=>{
+// })
+
+// for(let i=1;i<=20;i++){
+//     if (data.meals[0][`strIngredient${i}`] != "" && data.meals[0][`strIngredient${i}`]!=null){
+//     document.getElementById("Ingredi-list").innerHTML += `<li>${data.meals[0][`strIngredient${i}`]}</li>`;
+//   }}
